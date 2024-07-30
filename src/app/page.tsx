87119dbdd5e4ad373094e66228cb2ae4b0737fcd
Ghastly2001 <divyanshverma18may@gmail.com>
2024-07-30 21:41:54 +0530

    ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,21 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 export default function Home() {
   return (
-    <Wrapper>
-      <main className="flex flex-col items-start justify-center h-full text-4xl lg:text-7xl font-semibold lg:font-bold tracking-tight mt-10">
-        <span>Find Biggest NFT</span>
-        <div className="flex gap-2 w-full items-center">
-          <span>Digital Artwork</span>
-          <img
-            src="/assets/verified.png"
-            alt="verified"
-            className="w-12 h-12"
-          />
+    <Wrapper className="flex justify-start items-start  h-screen lg:pt-24">
+      <main className="flex flex-col items-start text-4xl lg:text-7xl font-semibold lg:font-bold tracking-tight">
+        <div>
+          <span>Find Biggest NFT</span>
+          <div className="flex gap-2 w-full items-center">
+            <span>Digital Artwork</span>
+            <img
+              src="/assets/verified.png"
+              alt="verified"
+              className="w-12 h-12"
+            />
+          </div>
+          <span> in the market</span>
+          <div className="flex gap-2"></div>
         </div>
-        <div className="flex gap-2"></div>
-        <span> in the market</span>
       </main>
     </Wrapper>
   );
