8910e78fb6f48776936bbc8803ac3c7deefce7df
Ghastly2001 <divyanshverma18may@gmail.com>
2024-08-04 22:50:08 +0530

    cp

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20 fixed bottom-0 left-0">
       <div className="flex justify-between items-center py-2">
         <p className="text-sm text-muted-foreground">
-          &copy; {date.getFullYear()} All rights reserved
+          &copy;Flux {date.getFullYear()} All rights reserved
         </p>
       </div>
     </footer>
