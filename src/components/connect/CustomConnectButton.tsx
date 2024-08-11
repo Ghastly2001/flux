73115fa204c17da47281f850dce27aba60446eda
Ghastly2001 <divyanshverma18may@gmail.com>
2024-08-11 18:52:05 +0530

    connect wallet

--- src/components/connect/CustomConnectButton.tsx
+++ src/components/connect/CustomConnectButton.tsx
@@ -1,11 +1,28 @@
 import React from "react";
-import { ConnectButton } from "thirdweb/react";
-import { client } from "../../../utils/constants";
+import { ConnectButton, darkTheme } from "thirdweb/react";
+import { chain, client } from "../../../utils/constants";
 
 const CustomConnectButton = () => {
   return (
     <div>
-      <ConnectButton client={client} />
+      <ConnectButton
+        client={client}
+        chain={chain}
+        connectButton={{
+          label: "Connect Wallet",
+        }}
+        theme={darkTheme({
+          colors: {
+            accentText: "#DB2777",
+            accentButtonBg: "#DB2777",
+          },
+        })}
+        connectModal={{
+          size: "wide",
+          titleIcon: "",
+          showThirdwebBranding: false,
+        }}
+      />
     </div>
   );
 };
