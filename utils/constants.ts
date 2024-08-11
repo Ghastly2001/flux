c144f7dd09ebf4ccc6a13919562a99b78799d09e
Ghastly2001 <divyanshverma18may@gmail.com>
2024-08-11 18:02:24 +0530

    added chain

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,10 @@
-import { createThirdwebClient } from "thirdweb";
+import { createThirdwebClient, defineChain } from "thirdweb";
 
 const clientId = process.env.NEXT_PUBLIC_CLIENT_ID as string;
 export const client = createThirdwebClient({
   clientId,
 });
+
+const chainId = process.env.BASE_SEPOLIA_CHAIN_ID || 1;
+
+export const chain = defineChain(chainId as number);
